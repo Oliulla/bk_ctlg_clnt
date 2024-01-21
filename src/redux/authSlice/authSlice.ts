631e6a7f102ac8406bbe101c7913f93b360c0196
Oliulla <razasm29@gmail.com>
2024-01-22 02:13:13 +0600

    [STORED]: User info in redux auth store

--- src/redux/authSlice/authSlice.ts
+++ src/redux/authSlice/authSlice.ts
@@ -1,21 +1,64 @@
 import { createSlice } from "@reduxjs/toolkit";
 import Cookies from "js-cookie";
+import atob from "atob";
 
+const parseJwt = (token: string) => {
+  if (!token) return null;
+  try {
+    const base64Url = token.split(".")[1];
+    const base64 = base64Url.replace("-", "+").replace("_", "/");
+
+    // Check if atob is available
+    const decodedData =
+      typeof atob === "function"
+        ? atob(base64)
+        : Buffer.from(base64, "base64").toString("utf-8");
+
+    const userParseData = JSON.parse(decodedData);
+    // console.log(userParseData);
+    return userParseData;
+  } catch (error) {
+    console.error("Error parsing JWT:", error);
+    return null;
+  }
+};
+
+// Get initial token and refresh token from cookies
 const initialToken = Cookies.get("book-ctlg-accessToken") || null;
 
+// Fetch the refresh token after the initial rendering (e.g., in a useEffect)
+// const initialRefToken = Cookies.get("book-ctlg-refreshToken") || null;
+
+// console.log("initialRefToken:", initialToken);
+
+// Parse user details from the refresh token
+const initialUser = parseJwt(initialToken as string) || {
+  email: null,
+  secret: null,
+  expires_in: null,
+};
+
+// Create the auth slice
 const authSlice = createSlice({
   name: "auth",
   initialState: {
     token: initialToken,
+    user: initialUser,
   },
-
   reducers: {
     setToken: (state, action) => {
       state.token = action.payload;
     },
+    setUser: (state, action) => {
+      state.user = {
+        email: action.payload.email || null,
+        secret: action.payload.secret || null,
+        expires_in: action.payload.expires_in || null,
+      };
+    },
   },
 });
 
-export const { setToken } = authSlice.actions;
-
+// Export action creators and the reducer
+export const { setToken, setUser } = authSlice.actions;
 export default authSlice.reducer;
