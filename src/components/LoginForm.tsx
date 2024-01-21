36ab94838d6a6837c1c31528fee0213f74cc3242
Oliulla <razasm29@gmail.com>
2024-01-22 01:24:08 +0600

    [IMPLEMENTED]: Logout

--- src/components/LoginForm.tsx
+++ src/components/LoginForm.tsx
@@ -50,46 +50,48 @@ export default function LoginForm() {
   };
 
   return (
-    <div className="max-w-md mx-auto p-4 bg-white rounded shadow-md">
+    <>
       {isUserLoading ? (
         <Loader />
       ) : (
         <>
-          <h2 className="text-2xl font-semibold mb-4">Login</h2>
-          <form onSubmit={handleSubmit(onSubmit)}>
-            <div className="mb-4">
-              <label htmlFor="email" className="block font-medium">
-                Email
-              </label>
-              <input
-                type="email"
-                id="email"
-                className="w-full border rounded p-2"
-                {...register("email", { required: true })}
-              />
-            </div>
-            <div className="mb-4">
-              <label htmlFor="password" className="block font-medium">
-                Password
-              </label>
-              <input
-                type="password"
-                id="password"
-                className="w-full border rounded p-2"
-                {...register("password", { required: true })}
-              />
-            </div>
-            <div>
-              <button
-                type="submit"
-                className="bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-600"
-              >
-                Log In
-              </button>
-            </div>
-          </form>
+          <div className="max-w-md mx-auto p-4 bg-white rounded shadow-md">
+            <h2 className="text-2xl font-semibold mb-4">Login</h2>
+            <form onSubmit={handleSubmit(onSubmit)}>
+              <div className="mb-4">
+                <label htmlFor="email" className="block font-medium">
+                  Email
+                </label>
+                <input
+                  type="email"
+                  id="email"
+                  className="w-full border rounded p-2"
+                  {...register("email", { required: true })}
+                />
+              </div>
+              <div className="mb-4">
+                <label htmlFor="password" className="block font-medium">
+                  Password
+                </label>
+                <input
+                  type="password"
+                  id="password"
+                  className="w-full border rounded p-2"
+                  {...register("password", { required: true })}
+                />
+              </div>
+              <div>
+                <button
+                  type="submit"
+                  className="bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-600"
+                >
+                  Log In
+                </button>
+              </div>
+            </form>
+          </div>
         </>
       )}
-    </div>
+    </>
   );
 }
--- src/layouts/Navbar.tsx
+++ src/layouts/Navbar.tsx
@@ -1,6 +1,38 @@
-import { Link } from 'react-router-dom';
+import { useDispatch } from "react-redux";
+import { Link } from "react-router-dom";
+import Cookies from "js-cookie";
+import { setToken } from "../redux/authSlice/authSlice";
+import { useState } from "react";
+import { apiOperationMethods } from "../constants/apiOperationMethods";
+import { toast } from "react-toastify";
 
 export default function Navbar() {
+  const dispatch = useDispatch();
+  const [isLogOut, setIsLogOut] = useState<Boolean>(false);
+
+  // Handle admin logout
+  const handleSignOut = async () => {
+    dispatch(setToken(null));
+    Cookies.set("book-ctlg-accessToken", "");
+    // Cookies.set("book-ctlg-refreshToken", "", {
+    //   domain: "http://localhost:3000",
+    //   path: "/",
+    // });
+
+    const res = await fetch("http://localhost:5000/api/v1/auth/log-out", {
+      method: apiOperationMethods.POST,
+      credentials: "include",
+    });
+
+    // console.log("res -->", res);
+    const data = await res.json();
+
+    // console.log("res data -->", data);
+    toast.success(data?.message);
+
+    setIsLogOut(true);
+  };
+
   return (
     <nav className="bg-gray-900 p-4 shadow-md">
       <div className="container mx-auto flex justify-between items-center">
@@ -9,22 +41,38 @@ export default function Navbar() {
         </div>
         <ul className="flex space-x-4">
           <li>
-            <Link to="/all-books" className="text-white hover:underline">All Books</Link>
+            <Link to="/all-books" className="text-white hover:underline">
+              All Books
+            </Link>
           </li>
           <li>
-            <Link to="/add-new-book" className="text-white hover:underline">Add New Book</Link>
+            <Link to="/add-new-book" className="text-white hover:underline">
+              Add New Book
+            </Link>
           </li>
           <li>
-            <Link to="/sign-in" className="text-white hover:underline">Sign In</Link>
+            <Link to="/sign-in" className="text-white hover:underline">
+              Sign In
+            </Link>
           </li>
           <li>
-            <Link to="/sign-up" className="text-white hover:underline">Sign Up</Link>
+            <Link to="/sign-up" className="text-white hover:underline">
+              Sign Up
+            </Link>
           </li>
           <li>
-            <Link to="/my-profile" className="text-white hover:underline">My Profile</Link>
+            <Link to="/my-profile" className="text-white hover:underline">
+              My Profile
+            </Link>
           </li>
           <li>
-            <button type='submit' className="bg-red-900 text-white px-2">Sign Out</button>
+            <button
+              onClick={handleSignOut}
+              type="submit"
+              className="bg-red-900 text-white px-2"
+            >
+              Sign Out
+            </button>
           </li>
         </ul>
       </div>
